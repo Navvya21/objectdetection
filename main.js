1377c34543fb28b35747bbdf11eb32779c0184b2
Navvya21 <75784090+Navvya21@users.noreply.github.com>
2022-06-18 15:31:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img="";
 status ="";
+objects = [];
 
 function preload(){
     img = loadImage("dog_cat.jpg");
@@ -14,17 +15,20 @@ function setup(){
 
 function draw(){
 image(img, 0, 0, 640, 420);
-text("Dog", 45, 75);
-fill("#FF0000");
-noFill();
-stroke("#FF0000");
-rect(30, 60, 450, 350);
 
-fill("#FF0000");
-text("Cat", 320, 120);
-noFill();
-stroke("#FF0000");
-rect(300, 90, 270, 320);
+if(status !=""){
+    for(i = 0; i < objects.length; i++) {
+        document.getElementById("status").innerHTML = "Status : Object Detected";
+        fill("#FF0000");
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y +15);
+        noFill();
+        stroke("#FF0000");
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+    }
+}
+
 }
 
 function modelLoaded(){
@@ -38,4 +42,5 @@ function gotResult(error, results) {
         console.log(error);
     }
     console.log(results);
+    objects = results;
 }
